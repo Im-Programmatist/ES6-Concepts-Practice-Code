69d0b39815f7b7f496d94ef030eb2b06f7584bff
Chetan Korde <chetan.korde@maximess.com>
2022-08-09 15:15:40 +0530

    added new concept for this

--- ThisKeyword.js
+++ ThisKeyword.js
@@ -6,6 +6,7 @@ In JS 'this' keyword refers to the object it belongs to. It has different values
 	In regular function, this also refer to global object
 	In method/object, this refers to the owner object.
 	In a function, in strict mode this is undefind.
+	Usually this referes to global context if there is no nested function - (refers context to one leyer up) 
 */
 
 "use strict"
@@ -37,5 +38,45 @@ const object = {
 		childreg();
 	}
 } 
-
 object.sumobj();
+
+var a  = 100 ;
+function checkThisScope(){
+	var a = 200;
+	console.log(a);//200
+	console.log(this.a);//100	
+}
+
+const funcObj = {
+	arr : ["Java","Node","React","MongoDB"],
+	str : "Tutorial",
+	info(){ 
+		//this.arr is valid as it can refer the outer scope here
+		this.arr.forEach(function(value,index)
+		{	
+			//But this this.str will not reach out to the outer scope where this str defines 
+			//this.str find the variable inside only the info() function, there is no such declared variable available then
+			//this.str gives undefined like - 0. undefined Java, 1. undefined Node
+			console.log(index+". "+this.str+" "+value);
+		},this); 
+		/****TO GET ACCESS OF str IN FOREACH CALL BACK -
+		We must have to pass context of this to the foreach callback function then only above valiables in object can accessible in it. 
+		*/
+	}
+}
+funcObj.info(); //
+/* 
+Output is - without passing 'this to inner callback function'
+0. undefined Java
+1. undefined Node
+2. undefined React
+3. undefined MongoDB
+
+Output is - without passing 'this to inner callback function'
+0. Tutorial Java
+1. Tutorial Node
+2. Tutorial React
+3. Tutorial MongoDB
+
+*/
+
